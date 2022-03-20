57e5e63f9b000b956045b2e5504a2009215a4c68
oliviacowan <oliva.cowan@hotmail.com>
2022-03-20 13:57:44 -0700

    move confirm into file within componenets

---
+++ front-end/src/components/medicationActions/Confirm.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Confirm.scss";
+
+export default function Confirm(props) {
+  
+  const cancel = function () {
+    props.setDestroy(false);
+  };
+
+  return (
+    <div className="confirm__card">
+      <div className="message">Do you want to delete this item?</div>
+      <div className="confirm__card--buttons--container">
+        <button
+          className="confirm__card--buttons"
+          onClick={cancel}
+        >
+          Cancel
+        </button>
+        <button className="confirm__card--buttons">Delete</button>
+      </div>
+    </div>
+  );
+}
