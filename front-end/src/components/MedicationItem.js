ddb43b6246ef2a5491eb5727f02e73d453d4c91f
oliviacowan <oliva.cowan@hotmail.com>
2022-03-20 13:58:30 -0700

    render delete on click. on cancel go back to medication

--- front-end/src/components/MedicationItem.js
+++ front-end/src/components/MedicationItem.js
@@ -1,36 +1,49 @@
-
+import react, { useState } from "react";
+import Confirm from "./medicationActions/Confirm";
 import "./MedicationItem.scss";
 
+export default function MedicationItem({ time, name, id, child }) {
+  const [destroy, setDestroy] = useState(false);
 
-export default function MedicationItem({ time, name, id, child}) {
-  
   function getInfo() {
     console.log("clicked info icon", id);
   }
-  function destroy() {
-    console.log("clicked delete icon", id);
-  }
+ 
+  const destroyBoolean = function () {
+    if (!destroy) {
+      setDestroy(true);
+    }
+  };
+
   function edit() {
     console.log("clicked edit icon", id);
   }
 
   return (
-    <li className="medication-item">
-      <div className="medication-time-name">
-        <p className="scheduled-time">{time}   <i class="fa-solid fa-arrow-right-long"></i>   {child}</p>
-        <h2 className="medication-name">{name}</h2>
-      </div>
-      <section className="medication-item-icons">
-        <p onClick={edit}>
-          <i className="fa-solid fa-user-pen"></i>
-        </p>
-        <p onClick={destroy}>
-          <i className="fa-solid fa-trash"></i>{" "}
-        </p>
-        <p onClick={getInfo}>
-          <i className="fa-solid fa-circle-info"></i>
-        </p>
-      </section>
-    </li>
+    <>
+      {destroy ? (
+        <Confirm destroy={destroy} setDestroy={setDestroy} />
+      ) : (
+        <li className="medication-item">
+          <div className="medication-time-name">
+            <p className="scheduled-time">
+              {time} <i className="fa-solid fa-arrow-right-long"></i> {child}
+            </p>
+            <h2 className="medication-name">{name}</h2>
+          </div>
+          <section className="medication-item-icons">
+            <p onClick={edit}>
+              <i className="fa-solid fa-user-pen"></i>
+            </p>
+            <p>
+              <i className="fa-solid fa-trash" onClick={destroyBoolean}></i>
+            </p>
+            <p onClick={getInfo}>
+              <i className="fa-solid fa-circle-info"></i>
+            </p>
+          </section>
+        </li>
+      )}
+    </>
   );
 }
