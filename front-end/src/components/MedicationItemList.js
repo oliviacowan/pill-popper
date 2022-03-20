e53fc79d1670cc8e8c9c432550cfec337960aefa
oliviacowan <oliva.cowan@hotmail.com>
2022-03-20 10:58:22 -0700

    format date and only render if it matches calendar date

--- front-end/src/components/MedicationItemList.js
+++ front-end/src/components/MedicationItemList.js
@@ -1,33 +1,45 @@
 import React from "react";
-import MedicationItem from './MedicationItem'
+import MedicationItem from "./MedicationItem";
 
 const medications = [
   {
-    name: 'antibiotic',
-    time: '4pm',
-    id: 1
+    name: "antibiotic",
+    time: "4pm",
+    id: 1,
+    date: "03/20/2022",
   },
   {
-    name: 'tylenol',
-    time: '3pm',
-    id: 2
-  }
-]
+    name: "tylenol",
+    time: "3pm",
+    id: 2,
+    date: "11/19/2022",
+  },
+];
 
+export default function MedicationItemList(props) {
+  const medicationDate = props.date.toLocaleString().substring(0, 10);
+  const zero = "0";
 
-export default function MedicationItemList() {
+  const formatDate = function () {
+    if (props.date.getMonth() < 10) {
+      return zero.concat(medicationDate).slice(0, -1);
+    } else {
+      return medicationDate;
+    }
+  };
 
+  const medicationItemList = medications.map(
+    (medication) =>
+      medication.date === formatDate(medicationDate) && (
+        <MedicationItem
+          key={medication.id}
+          id={medication.id}
+          time={medication.time}
+          name={medication.name}
+          date={medication.date}
+        />
+      )
+  );
 
-  const medicationItemList = medications.map((medication) => (
-   <MedicationItem 
-      key={medication.id}
-      id={medication.id}
-      time={medication.time}
-      name={medication.name}
-      />
-
-  ));
-
-  return <>{medicationItemList}</>
-
+  return <>{medicationItemList}</>;
 }
