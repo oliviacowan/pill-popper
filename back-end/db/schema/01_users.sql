a303ca00c773cee16114f669da5ecad3207efbd8
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-03-20 13:17:38 -0700

    Fix syntax errors

--- back-end/db/schema/01_users.sql
+++ back-end/db/schema/01_users.sql
@@ -5,6 +5,6 @@ CREATE TABLE users (
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
-  avatar_url VARCHAR(1000) NOT NULL,
+  avatar_url VARCHAR(1000) NOT NULL
 );
 
