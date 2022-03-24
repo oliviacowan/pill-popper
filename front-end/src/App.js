e1f489202c676d141d2524ce7cba3b806282ffa0
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-03-24 09:41:44 -0700

    Add function medEndDate

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -38,6 +38,7 @@ function App(props) {
     children: {},
   });
 
+
   const hasValue = Object.keys(state.children).length !== 0;
 
   const setSectedChild = (child) => setState({ ...state, child });
@@ -61,7 +62,8 @@ function App(props) {
     axios
       .get("users/1/medications")
       .then((response) => {
-        // console.log(response);
+
+        console.log(response);
         setMedications((prev) => [
           {
             ...prev,
--- front-end/src/components/Form.js
+++ front-end/src/components/Form.js
@@ -86,9 +86,9 @@ export default function Form(props) {
     }))
   }
 
-  const timeList = savedTime.map(time => (
-    <div>
-     <span>{ time }</span>
+  const timeList = savedTime.map( time => (
+    <div> 
+     <span> { time }</span>
       <button onClick={() => handleRemoveTime(time)}>x</button>
      
     </div>
